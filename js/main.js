476294516f7e1e0487780fa5e97051714f39caa3
huyhuynh408 <huyhuynh85@gmail.com>
2015-09-02 10:51:46 -0500

    added js for form submission

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$(window).ready(function(){
+  $('#homeowner').click(function(){
+    $('form').attr('action', "http://mygreenvillage.us11.list-manage.com/subscribe/post?u=928bc9fdf91abfab45016cd37&amp;id=cbc38ceb0d");
+  });
 
+  $('#provider').click(function(){
+    $('form').attr('action', "http://mygreenvillage.us11.list-manage.com/subscribe/post?u=928bc9fdf91abfab45016cd37&amp;id=918fef1206");
+  });
+});
